20ac7f810d059dc246da7b62f7972906c4d8904d
Simon Dückert <simon.dueckert@cogneon.de>
2025-03-05 15:40:51 +0100

    Kapitel KI und maschinelles Lernen überarbeitet #77

--- de/make.bat
+++ de/make.bat
@@ -2,9 +2,9 @@
 echo Starting lernOS Guide Generation ...
 
 REM Variables
-set filename="lernOS-Template-Guide-de"
+set filename="lernos-ai-guide"
 REM set chapters="./src/index.md ./src/1-0-Grundlagen.md ./src/1-1-Grundidee.md ./src/1-2-Lebenszyklus.md ./src/1-3-Inhaltsverzeichnis.md ./src/1-4-Inhalt.md ./src/1-5-Produktionskette.md ./src/2-Lernpfad.md ./src/3-Anhang.md"
-set chapters=./src/index.md ./src/1-0-Grundlagen.md ./src/1-1-Grundidee.md ./src/1-2-Lebenszyklus.md ./src/1-3-Inhaltsverzeichnis.md ./src/1-4-Inhalt-Schreibstil.md ./src/1-5-Markdown-Syntax.md ./src/1-6-Produktionskette.md ./src/1-7-Github.md ./src/2-0-Lernpfad.md ./src/3-0-Anhang.md
+set chapters="./src/index.md ./src/1-00-basics.md"
 
 REM Delete Old Versions
 echo Deleting old versions ...
@@ -37,3 +37,5 @@ REM magick mogrify -crop 1563x2500+102+0 src/images/ebook-cover.jpg
 REM pandoc metadata.yaml --from markdown -s --resource-path="./src" -F mermaid-filter --epub-cover-image=src/images/ebook-cover.jpg --number-sections --toc -V lang=de-de -o %filename%.epub %chapters%
 pandoc metadata.yaml --from markdown -s --resource-path="./src" --number-sections --toc -V lang=de-de -o %filename%.epub %chapters%
 REM ebook-convert %filename%.epub %filename%.mobi
+
+pause
